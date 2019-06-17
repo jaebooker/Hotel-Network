1506688c643ac694a7ab4a99336fffe855868a92
jaebooker <jaesonbooker@gmail.com>
2019-06-17 12:06:55 -0700

    Initial Commit

---
+++ Hotel Network/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Hotel Network
+//
+//  Created by Jaeson Booker on 6/17/19.
+//  Copyright © 2019 Jaeson Booker. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
